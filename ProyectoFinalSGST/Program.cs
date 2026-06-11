4196520a0ef955cc26942fbdb6143a657a3ffbd3
Maykel <maykelcruz@uamv.edu.ni>
2026-06-11 14:24:07 -0600

    Feat: Integración total de la gestion de incidencias + reparacion y ajustación del codigo y variables/struct para el funcionamiento correcto general

--- ProyectoFinalSGST/Program.cs
+++ ProyectoFinalSGST/Program.cs
@@ -1,4 +1,9 @@
-﻿int MainMenu()
+﻿using System;
+
+Incidencia[] incidencias = new Incidencia[100];
+int cantidad = 0;
+
+int MainMenu()
 {
     Console.ForegroundColor = ConsoleColor.DarkBlue;
     Console.WriteLine("=== SISTEMA DE SOPORTE TECNOLOGIA UAM ===");
@@ -51,13 +56,13 @@ int MenuReportes()
     Console.ForegroundColor = ConsoleColor.DarkCyan;
     Console.WriteLine("=== REPORTES Y ESTADÍSTICAS ===");
     Console.WriteLine("1. Reporte general");
-    Console.WriteLine("1. Incidencias por categoria");
-    Console.WriteLine("1. Incidencias por prioridad");
-    Console.WriteLine("1. Incidencias por edificio");
-    Console.WriteLine("1. Incidencias por piso");
-    Console.WriteLine("1. Técnico mas ocupado");
-    Console.WriteLine("1. Técnico menos ocupado");
-    Console.WriteLine("1. Aula con mas reportes");
+    Console.WriteLine("2. Incidencias por categoria");
+    Console.WriteLine("3. Incidencias por prioridad");
+    Console.WriteLine("4. Incidencias por edificio");
+    Console.WriteLine("5. Incidencias por piso");
+    Console.WriteLine("6. Técnico mas ocupado");
+    Console.WriteLine("7. Técnico menos ocupado");
+    Console.WriteLine("8. Aula con mas reportes");
     Console.WriteLine("0. Regresar");
     Console.ForegroundColor = ConsoleColor.DarkYellow;
     Console.Write("Digite su opción: ");
@@ -65,4 +70,270 @@ int MenuReportes()
     return int.Parse(Console.ReadLine()!);
 }
 
- 
+// GESTION DE INCIDENCIAS
+
+void RegistrarIncidencia()
+{
+    Console.Clear();
+
+    Console.Write("Codigo: ");
+    incidencias[cantidad].Codigo = Console.ReadLine()!;
+
+    Console.Write("Nombre del reportante: ");
+    incidencias[cantidad].Reportante = Console.ReadLine()!;
+
+    Console.Write("Tipo de usuario: ");
+    incidencias[cantidad].TipoUsuario = Console.ReadLine()!;
+
+    Console.Write("Aula: ");
+    incidencias[cantidad].Aula = Console.ReadLine()!;
+
+    Console.Write("Categoria: ");
+    incidencias[cantidad].Categoria = Console.ReadLine()!;
+
+    Console.Write("Descripcion: ");
+    incidencias[cantidad].Descripcion = Console.ReadLine()!;
+
+    Console.Write("Fecha: ");
+    incidencias[cantidad].Fecha = Console.ReadLine()!;
+
+    Console.Write("Prioridad: ");
+    incidencias[cantidad].Prioridad = Console.ReadLine()!;
+
+    Console.Write("Estado: ");
+    incidencias[cantidad].Estado = Console.ReadLine()!;
+
+    cantidad++;
+
+    Console.WriteLine("\nIncidencia registrada correctamente.");
+    Console.ReadKey();
+}
+
+void BuscarIncidencia()
+{
+    Console.Clear();
+
+    Console.Write("Ingrese codigo: ");
+    string buscar = Console.ReadLine()!;
+
+    bool encontrado = false;
+
+    for (int i = 0; i < cantidad; i++)
+    {
+        if (incidencias[i].Codigo == buscar)
+        {
+            Console.WriteLine("\nCodigo: " + incidencias[i].Codigo);
+            Console.WriteLine("Reportante: " + incidencias[i].Reportante);
+            Console.WriteLine("Tipo Usuario: " + incidencias[i].TipoUsuario);
+            Console.WriteLine("Aula: " + incidencias[i].Aula);
+            Console.WriteLine("Categoria: " + incidencias[i].Categoria);
+            Console.WriteLine("Descripcion: " + incidencias[i].Descripcion);
+            Console.WriteLine("Fecha: " + incidencias[i].Fecha);
+            Console.WriteLine("Prioridad: " + incidencias[i].Prioridad);
+            Console.WriteLine("Estado: " + incidencias[i].Estado);
+
+            encontrado = true;
+            break;
+        }
+    }
+
+    if (!encontrado)
+    {
+        Console.WriteLine("Incidencia no encontrada.");
+    }
+
+    Console.ReadKey();
+}
+
+void ModificarIncidencia()
+{
+    Console.Clear();
+
+    Console.Write("Codigo a modificar: ");
+    string buscar = Console.ReadLine()!;
+
+    bool encontrado = false;
+
+    for (int i = 0; i < cantidad; i++)
+    {
+        if (incidencias[i].Codigo == buscar)
+        {
+            Console.Write("Nuevo estado: ");
+            incidencias[i].Estado = Console.ReadLine()!;
+
+            Console.Write("Nueva prioridad: ");
+            incidencias[i].Prioridad = Console.ReadLine()!;
+
+            Console.Write("Nueva descripcion: ");
+            incidencias[i].Descripcion = Console.ReadLine()!;
+
+            Console.WriteLine("\nIncidencia modificada.");
+
+            encontrado = true;
+            break;
+        }
+    }
+
+    if (!encontrado)
+    {
+        Console.WriteLine("Incidencia no encontrada.");
+    }
+
+    Console.ReadKey();
+}
+
+void EliminarIncidencia()
+{
+    Console.Clear();
+
+    Console.Write("Codigo a eliminar: ");
+    string buscar = Console.ReadLine()!;
+
+    bool encontrado = false;
+
+    for (int i = 0; i < cantidad; i++)
+    {
+        if (incidencias[i].Codigo == buscar)
+        {
+            for (int j = i; j < cantidad - 1; j++)
+            {
+                incidencias[j].Codigo = incidencias[j + 1].Codigo;
+                incidencias[j].Reportante = incidencias[j + 1].Reportante;
+                incidencias[j].TipoUsuario = incidencias[j + 1].TipoUsuario;
+                incidencias[j].Aula = incidencias[j + 1].Aula;
+                incidencias[j].Categoria = incidencias[j + 1].Categoria;
+                incidencias[j].Descripcion = incidencias[j + 1].Descripcion;
+                incidencias[j].Fecha = incidencias[j + 1].Fecha;
+                incidencias[j].Prioridad = incidencias[j + 1].Prioridad;
+                incidencias[j].Estado = incidencias[j + 1].Estado;
+            }
+
+            cantidad--;
+            encontrado = true;
+
+            Console.WriteLine("\nIncidencia eliminada.");
+            break;
+        }
+    }
+
+    if (!encontrado)
+    {
+        Console.WriteLine("Incidencia no encontrada.");
+    }
+
+    Console.ReadKey();
+}
+
+void MostrarIncidencias()
+{
+    Console.Clear();
+
+    if (cantidad == 0)
+    {
+        Console.WriteLine("No hay incidencias registradas.");
+    }
+    else
+    {
+        for (int i = 0; i < cantidad; i++)
+        {
+            Console.WriteLine("\n====================");
+            Console.WriteLine("Codigo: " + incidencias[i].Codigo);
+            Console.WriteLine("Reportante: " + incidencias[i].Reportante);
+            Console.WriteLine("Tipo de Usuario: " + incidencias[i].TipoUsuario);
+            Console.WriteLine("Aula: " + incidencias[i].Aula);
+            Console.WriteLine("Categoria: " + incidencias[i].Categoria);
+            Console.WriteLine("Descripcion: " + incidencias[i].Descripcion);
+            Console.WriteLine("Fecha: " + incidencias[i].Fecha);
+            Console.WriteLine("Prioridad: " + incidencias[i].Prioridad);
+            Console.WriteLine("Estado: " + incidencias[i].Estado);
+        }
+    }
+
+    Console.ReadKey();
+}
+
+
+
+void Main()
+{
+    int opcionMain;
+    do
+    {
+        opcionMain = MainMenu();
+        switch (opcionMain)
+        {
+            case 1:
+                int opcionIncidencias;
+                do
+                {
+                    opcionIncidencias = MenuIncidencias();
+                    switch (opcionIncidencias)
+                    {
+                        case 1:
+                            RegistrarIncidencia();
+                            break;
+                        case 2:
+                            BuscarIncidencia();
+                            break;
+                        case 3:
+                            ModificarIncidencia();
+                            break;
+                        case 4:
+                            EliminarIncidencia();
+                            break;
+                        case 5:
+                            MostrarIncidencias();
+                            break;
+                        case 0:
+                            Console.WriteLine("Regresando al menú principal...");
+                            break;
+                        default:
+                            Console.WriteLine("Opción no válida. Intente nuevamente.");
+                            break;
+                    }
+                } while (opcionIncidencias != 0);
+                break;
+            case 2:
+                int opcionTecnicos;
+                do
+                {
+                    opcionTecnicos = MenuTecnicos();
+                } while (opcionTecnicos != 0);
+                break;
+            case 3:
+                int opcionReportes;
+                do
+                {
+                    opcionReportes = MenuReportes();
+                } while (opcionReportes != 0);
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 0:
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Saliendo del sistema...");
+                Console.ResetColor();
+                break;
+            default:
+                Console.WriteLine("Opción no válida. Intente nuevamente.");
+                break;
+        }
+    } while (opcionMain != 0);
+}
+
+Main();
+
+struct Incidencia
+{
+    public string? Codigo;
+    public string Reportante;
+    public string TipoUsuario;
+    public string Aula;
+    public string Categoria;
+    public string Descripcion;
+    public string Fecha;
+    public string Prioridad;
+    public string Estado;
+}
